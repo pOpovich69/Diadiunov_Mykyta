d050449d7dcd69c8103c346fc46df30bc5f0ee08
Mykyta <myk23ta@gmail.com>
2023-03-03 10:54:36 +0200

    Create simple java program

---
+++ Task01/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /**
+     * Program entry point
+     * @param args  command-line parameters list
+     */
+    public static void main(String[] args){
+        for(String s: args){
+            System.out.println(s);
+        }
+    }
+}
